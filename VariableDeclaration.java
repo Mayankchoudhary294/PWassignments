445f1c28bbd6ed1456894b2e6bdd7faaacea6c29
Mayankchoudhary <mayankchoudhary4302@gmail.com>
2023-07-02 12:01:18 +0530

    Mock Test 1 - Java - PPT - PW Skills

---
+++ VariableDeclaration.java
@@ -0,0 +1,11 @@
+public class VariableDeclaration {
+    public static void main(String[] args) {
+        int myInt = 42;
+        double myDouble = 3.14;
+        String myString = "Hello, World!";
+
+        System.out.println("Value of myInt: " + myInt);
+        System.out.println("Value of myDouble: " + myDouble);
+        System.out.println("Value of myString: " + myString);
+    }
+}
